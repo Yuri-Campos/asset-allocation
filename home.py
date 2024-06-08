0896aab3a191a92847d9407a2db2287702e1191f
Yuri-Campos <ygsoares20@gmail.com>
2024-06-07 22:50:32 -0300

    configure home layout

--- home.py
+++ home.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="💸",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+st.markdown('# Home')
+st.sidebar.markdown('Made by [Yuri Campos](https://github.com/Yuri-Campos)')
+
+
