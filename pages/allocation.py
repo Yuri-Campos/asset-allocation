38da70096d57a26d7fc77d3d8753b2e859c56d50
Yuri-Campos <ygsoares20@gmail.com>
2024-06-07 22:51:26 -0300

    setup allocation layout

--- pages/allocation.py
+++ pages/allocation.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Asset Allocator",
+    page_icon="💸",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+st.markdown('# Asset Allocation')
+st.sidebar.markdown('Made by [Yuri Campos](https://github.com/Yuri-Campos)')
